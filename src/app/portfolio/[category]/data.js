b03809df8076853815e0a074986323a2a9f9ea66
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-10-10 14:01:07 +0100

    Fetch data by category

---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,71 @@
+export const items = {
+    applications: [
+      {
+        id: 1,
+        title: "Restaurant App",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+        image:
+          "https://images.unsplash.com/photo-1601972602237-8c79241e468b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80",
+      },
+      {
+        id: 2,
+        title: "Green App",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+        image:
+          "https://images.unsplash.com/photo-1601972599720-36938d4ecd31?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80",
+      },
+      {
+        id: 3,
+        title: "Management Social",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+        image:
+          "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2426&q=80",
+      }
+    ],
+    illustrations: [
+      {
+        id: 1,
+        title: "Creative Pills",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+        image:
+          "https://plus.unsplash.com/premium_photo-1675209695946-b1ce701b88fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2612&q=80",
+      },
+      {
+        id: 2,
+        title: "Minimal Single Home",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+        image:
+          "https://images.unsplash.com/photo-1436397543931-01c4a5162bdb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1919&q=80",
+      },
+      {
+        id: 3,
+        title: "Strong Green Loft",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+        image:
+          "https://images.unsplash.com/photo-1571215682742-561893604b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2576&q=80",
+      }
+    ],
+    websites: [
+      {
+        id: 1,
+        title: "Social Media",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+        image:
+          "https://plus.unsplash.com/premium_photo-1683488869706-4152826ca919?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80",
+      },
+      {
+        id: 2,
+        title: "Youtube",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+        image:
+          "https://plus.unsplash.com/premium_photo-1683842640591-210c38313e2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80",
+      },
+      {
+        id: 3,
+        title: "Strong Together Tag",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+        image:
+          "https://images.unsplash.com/photo-1513118172236-00b7cc57e1fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2380&q=80",
+      }
+    ],
+  };
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,57 +2,43 @@ import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import Button from "@/components/Button/Button";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+// Get all data for a particular category
+const getData = (cat) => {
+  const data = items[cat];
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
+
 const Category = ({ params }) => {
-  console.log(params);
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h2>Creative Portfolio</h2>
-          <p className={styles.desc}>
-            Lorsqu'il dit que "ceux qui sont conduits par l'Esprit sont libres
-            par rapport à la loi mosaïque," il signifie que la foi en Christ
-            change fondamentalement la manière dont les croyants sont justifiés.
-            Ils ne sont plus sous la condamnation de la loi, car Christ a
-          </p>
-          <Button url="#" text="See More" />
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h2>{item.title}</h2>
+            <p className={styles.desc}>
+            {item.desc}
+            </p>
+            <Button url="#" text="See More" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.image}
+              fill={true}
+              src={item.image}
+              alt=""
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image className={styles.image} fill={true} src="https://plus.unsplash.com/premium_photo-1661369089329-d89031837cdf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80" alt="" />
-        </div>
-      </div>
-
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h2>Creative Portfolio</h2>
-          <p className={styles.desc}>
-            Lorsqu'il dit que "ceux qui sont conduits par l'Esprit sont libres
-            par rapport à la loi mosaïque," il signifie que la foi en Christ
-            change fondamentalement la manière dont les croyants sont justifiés.
-            Ils ne sont plus sous la condamnation de la loi, car Christ a
-          </p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image className={styles.image} fill={true} src="https://plus.unsplash.com/premium_photo-1661369089329-d89031837cdf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80" alt="" />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h2>Creative Portfolio</h2>
-          <p className={styles.desc}>
-            Lorsqu'il dit que "ceux qui sont conduits par l'Esprit sont libres
-            par rapport à la loi mosaïque," il signifie que la foi en Christ
-            change fondamentalement la manière dont les croyants sont justifiés.
-            Ils ne sont plus sous la condamnation de la loi, car Christ a
-          </p>
-          <Button url="#" text="See More" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image className={styles.image} fill={true} src="https://plus.unsplash.com/premium_photo-1661369089329-d89031837cdf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80" alt="" />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
